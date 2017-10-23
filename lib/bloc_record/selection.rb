010bc08e3dd96eeab1963f203b03dafda3090f6a
xortiz <xavier.ortiz.ch@gmail.com>
2017-10-23 16:29:31 -0400

    some checkpoint changes?

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -1,174 +1,172 @@
 require 'sqlite3'
 
 module Selection
-  def find(*ids)
-    if ids.length == 1
-      find_one(ids.first)
-    else
-      rows = connection.execute <<-SQL
-      SELECT #{columns.join ","} FROM #{table}
-      WHERE id IN (#{ids.join(",")});
-      SQL
+	def find(*ids)
+		if ids.length == 1
+			find_one(ids.first)
+		else
+			rows = connection.execute <<-SQL
+	  SELECT #{columns.join ","} FROM #{table}
+	  WHERE id IN (#{ids.join(",")});
+			SQL
 
-      rows_to_array(rows)
-    end
-  end
+			rows_to_array(rows)
+		end
+	end
 
-  def find_one(id)
-    row = connection.get_first_row <<-SQL
-    SELECT #{columns.join ","} FROM #{table}
-    WHERE id = #{id};
-    SQL
+	def find_one(id)
+		row = connection.get_first_row <<-SQL
+	SELECT #{columns.join ","} FROM #{table}
+	WHERE id = #{id};
+		SQL
 
-    init_object_from_row(row)
-  end
+		init_object_from_row(row)
+	end
 
-  def find_by(attribute, value)
-    row = connection.get_first_row <<-SQL
-    SELECT #{columns.join ","} FROM #{table}
-    WHERE #{attribute} = #{BlocRecord::Utility.sql_strings(value)};
-    SQL
+	def find_by(attribute, value)
+		row = connection.get_first_row <<-SQL
+	SELECT #{columns.join ","} FROM #{table}
+	WHERE #{attribute} = #{BlocRecord::Utility.sql_strings(value)};
+		SQL
 
-    init_object_from_row(row)
-  end
+		init_object_from_row(row)
+	end
 
-  def take_one
-    row = connection.get_first_row <<-SQL
-    SELECT #{columns.join ","} FROM #{table}
-    ORDER BY random()
-    LIMIT 1;
-    SQL
+	def take_one
+		row = connection.get_first_row <<-SQL
+	SELECT #{columns.join ","} FROM #{table}
+	ORDER BY random()
+	LIMIT 1;
+		SQL
 
-    init_object_from_row(row)
-  end
+		init_object_from_row(row)
+	end
 
-  def take(num=1)
-    if num > 1
-      rows = connection.execute <<-SQL
-      SELECT #{columns.join ","} FROM #{table}
-      ORDER BY random()
-      LIMIT #{num};
-      SQL
+	def take(num=1)
+		if num > 1
+			rows = connection.execute <<-SQL
+	  SELECT #{columns.join ","} FROM #{table}
+	  ORDER BY random()
+	  LIMIT #{num};
+			SQL
 
-      rows_to_array(rows)
-    else
-      take_one
-    end
-  end
+			rows_to_array(rows)
+		else
+			take_one
+		end
+	end
 
-  def first
-    row = connection.get_first_row <<-SQL
-    SELECT #{columns.join ","} FROM #{table}
-    ORDER BY id
-    ASC LIMIT 1;
-    SQL
+	def first
+		row = connection.get_first_row <<-SQL
+	SELECT #{columns.join ","} FROM #{table}
+	ORDER BY id
+	ASC LIMIT 1;
+		SQL
 
-    init_object_from_row(row)
-  end
+		init_object_from_row(row)
+	end
 
-  def last
-    row = connection.get_first_row <<-SQL
-    SELECT #{columns.join ","} FROM #{table}
-    ORDER BY id
-    DESC LIMIT 1;
-    SQL
+	def last
+		row = connection.get_first_row <<-SQL
+	SELECT #{columns.join ","} FROM #{table}
+	ORDER BY id
+	DESC LIMIT 1;
+		SQL
 
-    init_object_from_row(row)
-  end
+		init_object_from_row(row)
+	end
 
-  def all
-    rows = connection.execute <<-SQL
-    SELECT #{columns.join ","} FROM #{table};
-    SQL
+	def all
+		rows = connection.execute <<-SQL
+	SELECT #{columns.join ","} FROM #{table};
+		SQL
 
-    rows_to_array(rows)
-  end
+		rows_to_array(rows)
+	end
 
-  def where(*args)
-    if args.count > 1
-      expression = args.shift
-      params = args
+	def where(*args)
+		if args.count > 1
+			expression = args.shift
+			params = args
 
-    else
-      case args.first
-      when String
-        expression = args.first
+		else
+			case args.first
+			when String
+				expression = args.first
 
-      when Hash
-        expression_hash = BlocRecord::Utility.convert_keys(args.first)
-        expression = expression_hash.map {|key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
-      end
-    end
+			when Hash
+				expression_hash = BlocRecord::Utility.convert_keys(args.first)
+				expression = expression_hash.map {|key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
+			end
+		end
 
-    sql = <<-SQL
-    SELECT #{columns.join ","} FROM #{table}
-    WHERE #{expression};
-    SQL
+		sql = <<-SQL
+	SELECT #{columns.join ","} FROM #{table}
+	WHERE #{expression};
+		SQL
 
-    rows = connection.execute(sql, params)
-    rows_to_array(rows)
-  end
+		rows = connection.execute(sql, params)
+		rows_to_array(rows)
+	end
 
-  def order(*args)
-    args.map! {|arg| arg.is_a?(Hash) ? format_hash_to_SQL(arg) : arg}
-    if args.count > 1
-      order = args.join(", ")
-    else
-      order = args.first.to_s
-    end
+	def order(*args)
+		args.map! {|arg| arg.is_a?(Hash) ? format_hash_to_SQL(arg) : arg}
+		if args.count > 1
+			order = args.join(", ")
+		else
+			order = args.first.to_s
+		end
 
-    rows = connection.execute <<-SQL
-    SELECT * FROM #{table}
-    ORDER BY #{order};
-    SQL
-    rows_to_array(rows)
-  end
+		rows = connection.execute <<-SQL
+	SELECT * FROM #{table}
+	ORDER BY #{order};
+		SQL
+		rows_to_array(rows)
+	end
 
-  def format_hash_to_SQL arg
-    arg.map{|k, v| "#{k} #{v.to_s.upcase}"}.join(", ")
-  end
+	def format_hash_to_SQL arg
+		arg.map{|k, v| "#{k} #{v.to_s.upcase}"}.join(", ")
+	end
 
-  def join(*args)
-    if args.count > 1
-      joins = args.map { |arg| "INNER JOIN #{arg} ON #{arg}.#{table}_id = #{table}.id"}.join(" ")
-      rows = connection.execute <<-SQL
-      SELECT * FROM #{table} #{joins}
-      SQL
-    else
-      case args.first
-      when String
-        rows = connection.execute <<-SQL
-        SELECT * FROM #{table} #{BlocRecord::Utility.sql_strings(args.first)};
-        SQL
-      when Symbol
-        rows = connection.execute <<-SQL
-        SELECT * FROM #{table}
-        INNER JOIN #{args.first} ON #{args.first}.#{table}_id = #{table}.id
-        SQL
-      when Hash
-        joins = args.map {|arg| nested_join(arg) }.join(" ")
-        rows = connection.execute <<-SQL
-        SELECT * FROM #{table} #{joins}
-        SQL
-      end
+	def join(*args)
+		if args.count > 1
+			joins = args.map { |arg| "INNER JOIN #{arg} ON #{arg}.#{table}_id = #{table}.id"}.join(" ")
+			rows = connection.execute <<-SQL
+	  SELECT * FROM #{table} #{joins}
+			SQL
+		else
+			case args.first
+			when String
+				rows = connection.execute <<-SQL
+		SELECT * FROM #{table} #{BlocRecord::Utility.sql_strings(args.first)};
+				SQL
+			when Symbol
+				rows = connection.execute <<-SQL
+		SELECT * FROM #{table}
+				INNER JOIN #{args.first} ON #{args.first}.#{table}_id = #{table}.id
+				SQL
+			when Hash
+				joins = args.map {|arg| nested_join(arg) }.join(" ")
+				rows = connection.execute <<-SQL
+		SELECT * FROM #{table} #{joins}
+				SQL
+			end
 
-    end
-  end
-
-  rows_to_array(rows)
+		end
+	end
+	#rows_to_array(rows)
 end
 
 private
 def init_object_from_row(row)
-  if row
-    data = Hash[columns.zip(row)]
-    new(data)
-  end
+	if row
+		data = Hash[columns.zip(row)]
+		new(data)
+	end
 end
 
 def rows_to_array(rows)
-  collection = BlocRecord::Collection.new
-  rows.each { |row| collection << new(Hash[columns.zip(row)]) }
-  collection
-end
+	collection = BlocRecord::Collection.new
+	rows.each { |row| collection << new(Hash[columns.zip(row)]) }
+	collection
 end
