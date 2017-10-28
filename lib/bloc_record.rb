1a53a8424aa086ed7815417a37dcb1af0f8e36f8
xortiz <xavier.ortiz.ch@gmail.com>
2017-10-28 12:15:43 -0400

    assignment association stuff

--- lib/bloc_record.rb
+++ lib/bloc_record.rb
@@ -1,9 +1,13 @@
 module BlocRecord
-  def self.connect_to(filename)
-    @database_filename = filename
-  end
+	def self.connect_to(filename, platform)
+		if platform.to_s == "sqlite3"
+			@database_filename = "#{filename}.sqlite"
+		elsif platform.to_s == "pg"
+			@database_filename = "#{filename}"
+		end
+	end
 
-  def self.database_filename
-    @database_filename
-  end
+	def self.database_filename
+		@database_filename
+	end
 end
